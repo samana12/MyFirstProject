187e041b153f6c41dc0ae13f334d9650b8659cea
Shreesha P R <samanats123@gmail.com>
2022-11-10 20:11:38 -0500

    My first git commit

---
+++ sample.py
@@ -0,0 +1,27 @@
+import json
+a = 1
+b = "string"
+c = 1.0
+d = [1, 2]
+e = {"key": "value"}
+f = True
+g = False
+i = (1, 2)
+
+# if f:
+#     print("TRUE")
+#
+# if g:
+#     print("FALSE")
+
+count = 0
+# while count < 10:
+#     count = count + 1
+#     print("Looooooop {}".format(count))
+
+for i in range(1, 11):
+    print("Loooooop {}".format(i))
+
+# names = ["Samana", "Shreesha"]
+# for name in names:
+#     # print("I met {}".format(name))
